3514d8c9c819288450d38d1309b76d28bd1a8e7a
Sweetan Gleam Tauro <64632755+SweetanGleam2004@users.noreply.github.com>
2025-02-23 10:28:49 +0530

    Update

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {Link} from 'expo-router';
 
 export default function Index() {
   return (
@@ -9,7 +10,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/sign-in">Profile</Link>
+      <Link href="/sign-in">property</Link>
+
     </View>
   );
 }
