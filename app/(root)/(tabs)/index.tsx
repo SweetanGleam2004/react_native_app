68cb78d1301ad3aa8bdc61263a6d1fd1b4bf4069
Sweetan Gleam Tauro <64632755+SweetanGleam2004@users.noreply.github.com>
2025-02-24 19:28:20 +0530

    Update

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
